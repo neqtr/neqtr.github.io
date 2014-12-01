bb523ff15a7f9a3ae335533fdd5dacb7e2ed4f32
Wolfgang <wolf@okwolf.com>
2014-11-30 22:22:29 -0800

    adding jquery validation for great justice

--- js/app.js
+++ js/app.js
@@ -14,25 +14,28 @@ $(function() {
   $("body").track({ /* Optional Configuration */ });
   Parse.initialize('6uFTppb9Gpf0tLl1UlqF8wu4BS3FxQCfNXcZbdMe',
     'fVs9IbQfO5LZdKrSsmC4AcNtFVwQrh6M9FCuZZvi');
+  $("form").validate();
   $('form').submit(function(event) {
     event.preventDefault();
-    var LaunchSubscriber = Parse.Object.extend("LaunchSubscriber");
-    var $form = {};
-    $(this).serializeArray().map(function(x) {
-      $form[x.name] = x.value;
-    });
-    new LaunchSubscriber().save($form, {
-      success: function(object) {
-        $.growl.notice({
-          title: "Congrats!",
-          message: 'See ya when the app launches!'
-        });
-      },
-      error: function(model, error) {
-        $.growl.error({
-          message: 'Unable to sign you up to get notified!'
-        });
-      }
-    });
+    if (this.valid()) {
+      var LaunchSubscriber = Parse.Object.extend("LaunchSubscriber");
+      var $form = {};
+      $(this).serializeArray().map(function(x) {
+        $form[x.name] = x.value;
+      });
+      new LaunchSubscriber().save($form, {
+        success: function(object) {
+          $.growl.notice({
+            title: "Congrats!",
+            message: 'See ya when the app launches!'
+          });
+        },
+        error: function(model, error) {
+          $.growl.error({
+            message: 'Unable to sign you up to get notified!'
+          });
+        }
+      });
+    }
   });
 });
