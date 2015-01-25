9d546f6b990cf8f6a1657818d377aae6e5e1ed63
Wolfgang <wolf@okwolf.com>
2015-01-24 23:46:38 -0800

    adding shirt sales using stripe

--- js/app.js
+++ js/app.js
@@ -15,12 +15,11 @@ $(function() {
   Parse.initialize('6uFTppb9Gpf0tLl1UlqF8wu4BS3FxQCfNXcZbdMe',
     'fVs9IbQfO5LZdKrSsmC4AcNtFVwQrh6M9FCuZZvi');
   $("form").validate();
-  $(".content").fitVids();
   $('#privacy').toggle();
   $("#privacy-link").click(function() {
     $('#privacy').slideToggle(1000);
   });
-  $('form').submit(function(event) {
+  $('form.sign-up').submit(function(event) {
     event.preventDefault();
     if ($(this).valid()) {
       var LaunchSubscriber = Parse.Object.extend("LaunchSubscriber");
@@ -40,6 +39,53 @@ $(function() {
             message: 'Unable to sign you up to get notified!'
           });
         }
+      });
+    }
+  });
+  $('select.productSize').selectOrDie({
+    placeholderOption: true,
+    onChange: function() {
+      $(this).data('selectedValue', $(this).val());
+    }
+  });
+  var currentDescription = 'Unknown Item';
+  var checkout = StripeCheckout.configure({
+    key: 'pk_live_ukHMIqvSOCYc18IMkcSrDbik',
+    image: 'images/logo-padded.png',
+    token: function(token) {
+      $.post('https://neqtr-store.parseapp.com/orders', {
+        tokenId: token.id,
+        description: currentDescription,
+        email: token.email
+      }).done(function(data) {
+        console.log(data);
+        $.growl.notice({
+          title: data.result,
+          message: data.detail
+        });
+      })
+      .fail(function(data) {
+        $.growl.error({
+          message: data.responseText
+        });
+      })
+    }
+  });
+  $('form.buy-shirt').submit(function(event) {
+    event.preventDefault();
+    var selectedSize = $(this).find('select.productSize').data('selectedValue');
+    var description = selectedSize + ' ' + $(this).find('h5:first').text() + ' ($15.00)';
+    currentDescription = description;
+    if (selectedSize) {
+      checkout.open({
+        name: 'Neqtr',
+        description: description,
+        amount: 1500,
+        billingAddress: true
+      });
+    } else {
+      $.growl.error({
+        message: 'Please select a size!'
       });
     }
   });
